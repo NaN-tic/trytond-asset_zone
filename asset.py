95894b57663d5276a78cbde3f64b33cb917952d7
Guillem Barba <guillem@nan-tic.com>
2016-04-14 01:11:57 +0200

    Define metaclass at class level

--- asset.py
+++ asset.py
@@ -4,22 +4,25 @@ from trytond.model import fields, ModelSQL, ModelView
 from trytond.pool import PoolMeta
 
 __all__ = ['Zone', 'Employee', 'Asset']
-__metaclass__ = PoolMeta
 
 
 class Zone(ModelSQL, ModelView):
     'Zone'
     __name__ = 'asset.zone'
+    __metaclass__ = PoolMeta
     name = fields.Char('Name')
     employee = fields.Many2One('company.employee', 'Employee')
 
 
 class Employee:
     __name__ = 'company.employee'
-    zones = fields.One2Many('asset.zone', 'employee', 'Zones',
-        add_remove=[('employee', '=', None)])
+    __metaclass__ = PoolMeta
+    zones = fields.One2Many('asset.zone', 'employee', 'Zones', add_remove=[
+            ('employee', '=', None),
+            ])
 
 
 class Asset:
     __name__ = 'asset'
+    __metaclass__ = PoolMeta
     zone = fields.Many2One('asset.zone', 'Zone')
